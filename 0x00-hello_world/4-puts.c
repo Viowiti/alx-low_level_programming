4833505b3644a8a76ceaa5d0ae4af22ca51b8bd0
VictorOwiti <hunkyvee31@gmail.com>
2021-06-17 01:45:39 -0700

    Didplay string to std output using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - prints the string using the puts function to the standard output
+ *
+ *Return: the integer 0 on successful compilation
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
