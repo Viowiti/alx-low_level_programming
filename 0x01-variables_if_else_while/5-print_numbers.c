2c9ef53c432cc31a7e92498b357fb7c886b2f399
VictorOwiti <hunkyvee31@gmail.com>
2021-06-18 03:05:34 -0700

    print the base 10 integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints the decimal numbers 0-9
+ *
+ *Return: function doesnt have a return value
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");			
+}
