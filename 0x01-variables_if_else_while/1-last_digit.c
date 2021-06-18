a3829753b9004b27b470d5047e79884dc0c53574
VictorOwiti <hunkyvee31@gmail.com>
2021-06-18 02:07:49 -0700

    declaration separated from assignment

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,12 @@ int main(void)
 {
 	int n;
 
+	int lastNum;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int lastNum = n % 10;
+	lastNum = n % 10;
 
 	if (lastNum > 5)
 		printf("The last digit of %d is %d and is greater than 5\n", n, lastNum);
