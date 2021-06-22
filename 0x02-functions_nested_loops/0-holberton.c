8d8eedbdbc27c31301826cf760cb42ac3feed9e1
VictorOwiti <hunkyvee31@gmail.com>
2021-06-21 23:49:00 -0700

    print the string holberton using _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <holberton.h>
+/**
+ *main - prints out a string
+ *
+ *Return: 0 when compiled sucessfully.
+ */
+
+int main(void)
+{
+	_putchar("Holberton");
+	return (0);
+}
