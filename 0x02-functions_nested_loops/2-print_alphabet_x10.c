ff54f0487864f5f32d78cb975e66161f822a3a58
VictorOwiti <hunkyvee31@gmail.com>
2021-06-22 13:23:14 -0700

    removed the while loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,10 @@ void print_alphabet_x10(void)
 
 	j = 0;
 
-	while (j < 9)
+	for (j = 0; j <= 9; j++)
 	{
 		for (i = 97; i <= 122; i++)
 			_putchar(i);
-		_putchar('\n');
 	}
-	j++;
+	_putchar('\n');
 }
