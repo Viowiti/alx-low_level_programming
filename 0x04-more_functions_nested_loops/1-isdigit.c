2d084010f002d029a8540db8567c5aa1eee40624
VictorOwiti <hunkyvee31@gmail.com>
2021-06-24 03:22:36 -0700

    check if c is a decimal number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h>
+
+/**
+ * _isdigit - checks if c is a decimal number
+ * @c: The character being checked
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
