f468ce233f7b2c2b90e6a4029c667662f456eac0
VictorOwiti <hunkyvee31@gmail.com>
2021-06-24 03:50:00 -0700

    product of two untegers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ * @a:ine entry
+ * @b:second entry
+ * Return: product of the two numbers.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
