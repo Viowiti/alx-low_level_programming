b66d1d9598d34b920d8fb88681caa07e1c56154b
VictorOwiti <hunkyvee31@gmail.com>
2021-06-24 06:12:33 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
+#include "holberton.h"
+
 /**
- * print_numbers - writes the numbers 0-9 in stdout
- *
- * Return: No return value
- * On error, -1 is returned, and errno is set appropriately.
- */
+* print_numbers - prints numbers
+*
+* Return: void
+*/
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
