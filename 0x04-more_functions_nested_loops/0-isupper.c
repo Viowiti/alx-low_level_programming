07a76a6b6d2cfaf4f51bcc9b8a8b064e830a1682
VictorOwiti <hunkyvee31@gmail.com>
2021-06-24 02:13:29 -0700

    check if c is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _isupper - checks whether the value of c is an uppercase letter
+ * @c: The character being tested
+ *
+ * Return: On success 0
+ * On error, 1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (0);
+	else
+		return (1);
+}
