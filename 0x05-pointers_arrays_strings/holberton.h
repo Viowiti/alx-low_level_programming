2999eb9238640861564cffa78c48a52607449952
VictorOwiti <hunkyvee31@gmail.com>
2021-06-28 09:53:26 -0700

    reversing a string

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -9,4 +9,9 @@ void _puts(char *str);
 void print_array(int *a, int n);
 void print_rev(char *s);
 int length();
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 #endif /* HOLBERTON_H */
