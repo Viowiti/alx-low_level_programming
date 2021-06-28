ae84dcaa623dccbfe7f503b8fdc9c8d034ab5a9f
VictorOwiti <hunkyvee31@gmail.com>
2021-06-28 01:54:12 -0700

    reset n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * reset_to_98 - sets the value of n to 98
+ * @n: The pointer n is the sole parameter
+ *
+ * Return: No return value.
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
