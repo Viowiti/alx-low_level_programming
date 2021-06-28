dff72fe3301d9e45bf9a590439aa25995ebf311f
VictorOwiti <hunkyvee31@gmail.com>
2021-06-28 08:11:08 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * _print_rev - prints a string in reverse
+ * @s: the string pointer
+ *
+ * Return: no return value
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for(i = 0; s[i] <= '\0'; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
