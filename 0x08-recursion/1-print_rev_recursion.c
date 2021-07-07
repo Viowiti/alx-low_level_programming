6cbbadfb6379304e057c0d118f1b35b579cec1d6
VictorOwiti <hunkyvee31@gmail.com>
2021-07-07 02:49:39 -0700

    a function that prints a string in reverse.a function that prints a string in reverse.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,11 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 void _print_rev_recursion(char *s)
-{	
+{
 	if (*s == '\0')
 	{
 		return;
 	}
-
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
