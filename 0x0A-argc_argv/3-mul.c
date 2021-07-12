cd710bf3bbd2343e677b291d7392403a60ab6b9c
VictorOwiti <hunkyvee31@gmail.com>
2021-07-12 13:20:31 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that multiplies two arguments and prints the outcome
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul, num1, num2;
+
+	char Meso[] = "Error";
+
+	if (argc != 3)
+	{
+		printf("%s\n", Meso);
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[0]);
+		num2 = atoi(argv[1]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+	}
+}
