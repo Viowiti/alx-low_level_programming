661df530619a15498b7c708d39d555ae0f2a1b7e
VictorOwiti <hunkyvee31@gmail.com>
2021-07-29 12:43:57 -0700

    number of elements in list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - Calculates the length of a linked list
- * @h: Pointer to the head of the linked list
- * Return: The total amount of nodes in the linked list
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	int i;
+	size_t n = 0;
 
-	if (h == NULL)
-		return (0);
-	for (i = 1; h->next != NULL; i++)
+	while (h)
+	{
+		n++;
 		h = h->next;
-	return (i);
-}~
+	}
+	return (n);
+}
